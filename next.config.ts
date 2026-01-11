02090f2126245d250076a0ce9f1a1d2f1b164d49
vesperanocturne <vesperanocturne@gmail.com>
2026-01-11 16:53:04 +0530

    now it will work

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      }
+    }
+    return config
+  },
 }
 
 export default nextConfig
